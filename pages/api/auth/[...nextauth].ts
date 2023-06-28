c992f94f17ebf4c4f334f4fec6acd53cd3f03e55
Xys-Tus <Sixtus Onyedibe>
2023-06-28 16:18:27 +0100

    Ecommerce website

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+});
